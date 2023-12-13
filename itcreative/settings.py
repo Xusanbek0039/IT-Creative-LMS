7aa8c3d7e1d07b5ded8b50ba3aade1b4cb4a9410
Xusanbek Suyunov <109731657+Xusanbek0071@users.noreply.github.com>
2023-12-13 06:26:31 +0500

    a

--- itcreative/settings.py
+++ itcreative/settings.py
@@ -127,19 +127,17 @@ WSGI_APPLICATION = 'itcreative.wsgi.application'
 
 
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
+from pypika import Database
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),  
-        'PORT': os.getenv('DB_PORT'),
-    }
+  'default': {
+    'ENGINE': 'pypika.Database',
+    'HOST': os.environ['DB_HOST'],
+    'PORT': os.environ['DB_PORT'],  
+    'NAME': os.environ['DB_NAME'],
+    'USER': os.environ['DB_USER'],
+    'PASSWORD': os.environ['DB_PASSWORD']
+  }
 }
 
 
